4fa532f56b1834a385724786976f696e83175c1d
reponsesecurisee-pixel <reponsesecurisee@gmail.com>
2026-01-21 14:41:15 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Analytics } from '@vercel/analytics/react' // 1. Добавили импорт
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Analytics /> {/* 2. Вставили счетчик */}
   </React.StrictMode>,
 )
